baee0aca03b120d7fbeacc76d520a6ddf96af52a
lvcxss <lucas.oliveira.arcozelo@gmail.com>
2025-05-03 11:11:02 +0100

    feat: credits

--- src/components/windows/About.tsx
+++ src/components/windows/About.tsx
@@ -1,15 +1,49 @@
 export default function About() {
-  return <div>
-    <p>
-      Ballmer Notes is 3D note taking app for organizing your thoughts in space.
-    </p>
-    <p>
-      Imagine building your own digital memory palace where each note lives in a place you can see, explore, and return to anytime. Whether you're brainstorming ideas or studying for exams, Ballmer Notes helps you visualize and retain information more effectively. 
-    </p>
-    <p>
-      And the best part? You can access your entire space from your phone, wherever you are.
-    </p>
-      <br/>
+  return (
+    <div>
+      <p>
+        Ballmer Notes is 3D note taking app for organizing your thoughts in
+        space.
+      </p>
+      <p>
+        Imagine building your own digital memory palace where each note lives in
+        a place you can see, explore, and return to anytime. Whether you're
+        brainstorming ideas or studying for exams, Ballmer Notes helps you
+        visualize and retain information more effectively.
+      </p>
+      <p>
+        And the best part? You can access your entire space from your phone,
+        wherever you are.
+      </p>
+      <br />
       <p>Made for hackathon Shift APPens 2025 by four friend in 48h.</p>
-    </div>;
+
+      <br />
+      <p>Credits </p>
+      <p>
+        Heart4Modelling -
+        https://sketchfab.com/3d-models/isometric-base-for-my-detective-room-scene-9b99419b2c9940f5a73587e304ea486c
+      </p>
+      <br />
+      <p>
+        AdiSage -
+        https://sketchfab.com/3d-models/house-plant-4c4cfc7a848a45ada6e5facf2280ac3a
+      </p>
+      <br />
+      <p>
+        set_5001 -
+        https://sketchfab.com/3d-models/rug-f4a9c92c9e194abca7f84b8f1e4f8d2e
+      </p>
+      <br />
+      <p>
+        TroubleStudios -
+        https://sketchfab.com/3d-models/vintage-radio-df04a7c17b374a50b5290e64d8a12022
+      </p>
+      <br />
+      <p>
+        DDZ production -
+        https://sketchfab.com/3d-models/celling-lamp-dee2b4b1cbc64a2bbea0d6886fa36817
+      </p>
+    </div>
+  );
 }
